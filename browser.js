af0e50acc3df834524f1bce8cd57d723899de647
J. Harshbarger <hypercubed@gmail.com>
2015-11-12 15:00:29 +0900

    Build

---
+++ browser.js
@@ -0,0 +1,475 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+var svgStyles = { // Whitelist of CSS styles and default values
+  'alignment-baseline': 'auto',
+  'baseline-shift': 'baseline',
+  'clip': 'auto',
+  'clip-path': 'none',
+  'clip-rule': 'nonzero',
+  'color': 'rgb(51, 51, 51)',
+  'color-interpolation': 'srgb',
+  'color-interpolation-filters': 'linearrgb',
+  'color-profile': 'auto',
+  'color-rendering': 'auto',
+  'cursor': 'auto',
+  'direction': 'ltr',
+  'display': 'inline',
+  'dominant-baseline': 'auto',
+  'enable-background': '',
+  'fill': 'rgb(0, 0, 0)',
+  'fill-opacity': '1',
+  'fill-rule': 'nonzero',
+  'filter': 'none',
+  'flood-color': 'rgb(0, 0, 0)',
+  'flood-opacity': '1',
+  'font': '',
+  'font-family': 'normal',
+  'font-size': 'medium',
+  'font-size-adjust': 'auto',
+  'font-stretch': 'normal',
+  'font-style': 'normal',
+  'font-variant': 'normal',
+  'font-weight': '400',
+  'glyph-orientation-horizontal': '0deg',
+  'glyph-orientation-vertical': 'auto',
+  'image-rendering': 'auto',
+  'kerning': 'auto',
+  'letter-spacing': '0',
+  'lighting-color': 'rgb(255, 255, 255)',
+  'marker': '',
+  'marker-end': 'none',
+  'marker-mid': 'none',
+  'marker-start': 'none',
+  'mask': 'none',
+  'opacity': '1',
+  'overflow': 'visible',
+  'paint-order': 'fill',
+  'pointer-events': 'auto',
+  'shape-rendering': 'auto',
+  'stop-color': 'rgb(0, 0, 0)',
+  'stop-opacity': '1',
+  'stroke': 'none',
+  'stroke-dasharray': 'none',
+  'stroke-dashoffset': '0',
+  'stroke-linecap': 'butt',
+  'stroke-linejoin': 'miter',
+  'stroke-miterlimit': '4',
+  'stroke-opacity': '1',
+  'stroke-width': '1',
+  'text-anchor': 'start',
+  'text-decoration': 'none',
+  'text-rendering': 'auto',
+  'unicode-bidi': 'normal',
+  'visibility': 'visible',
+  'word-spacing': '0px',
+  'writing-mode': 'lr-tb'
+};
+
+var svgAttrs = [// white list of attributes
+'id', 'xml: base', 'xml: lang', 'xml: space', // Core
+'height', 'result', 'width', 'x', 'y', // Primitive
+'xlink: href', // Xlink attribute
+'style', 'class', 'd', 'pathLength', // Path
+'x', 'y', 'dx', 'dy', 'glyphRef', 'format', 'x1', 'y1', 'x2', 'y2', 'rotate', 'textLength', 'cx', 'cy', 'r', 'rx', 'ry', 'fx', 'fy', 'width', 'height', 'refX', 'refY', 'orient', 'markerUnits', 'markerWidth', 'markerHeight', 'maskUnits', 'transform', 'viewBox', 'version', // Container
+'preserveAspectRatio', 'xmlns', 'points', // Polygons
+'offset'];
+
+// http://www.w3.org/TR/SVG/propidx.html
+// via https://github.com/svg/svgo/blob/master/plugins/_collections.js
+var inheritableAttrs = ['clip-rule', 'color', 'color-interpolation', 'color-interpolation-filters', 'color-profile', 'color-rendering', 'cursor', 'direction', 'fill', 'fill-opacity', 'fill-rule', 'font', 'font-family', 'font-size', 'font-size-adjust', 'font-stretch', 'font-style', 'font-variant', 'font-weight', 'glyph-orientation-horizontal', 'glyph-orientation-vertical', 'image-rendering', 'kerning', 'letter-spacing', 'marker', 'marker-end', 'marker-mid', 'marker-start', 'pointer-events', 'shape-rendering', 'stroke', 'stroke-dasharray', 'stroke-dashoffset', 'stroke-linecap', 'stroke-linejoin', 'stroke-miterlimit', 'stroke-opacity', 'stroke-width', 'text-anchor', 'text-rendering', 'transform', 'visibility', 'white-space', 'word-spacing', 'writing-mode'];
+
+/* Some simple utilities */
+
+var isFunction = function isFunction(a) {
+  return typeof a === 'function';
+};
+var isDefined = function isDefined(a) {
+  return typeof a !== 'undefined';
+};
+var isUndefined = function isUndefined(a) {
+  return typeof a === 'undefined';
+};
+
+// detection
+var DownloadAttributeSupport = typeof document !== 'undefined' && 'download' in document.createElement('a');
+
+function saveUri(uri, name) {
+  if (DownloadAttributeSupport) {
+    var dl = document.createElement('a');
+    dl.setAttribute('href', uri);
+    dl.setAttribute('download', name);
+    dl.click();
+    return true;
+  } else if (typeof window !== 'undefined') {
+    window.open(uri, '_blank', '');
+    return true;
+  }
+
+  return false;
+}
+
+function savePng(uri, name) {
+  var canvas = document.createElement('canvas');
+  var context = canvas.getContext('2d');
+
+  var image = new Image();
+  image.onload = function () {
+    canvas.width = image.width;
+    canvas.height = image.height;
+    context.drawImage(image, 0, 0);
+
+    if (isDefined(window.saveAs) && isDefined(canvas.toBlob)) {
+      canvas.toBlob(function (blob) {
+        saveAs(blob, name);
+      });
+    } else {
+      saveUri(canvas.toDataURL('image/png'), name);
+    }
+  };
+  image.src = uri;
+  return true;
+}
+
+var _isDefined = function _isDefined(a) {
+  return typeof a !== 'undefined';
+};
+var _isUndefined = function _isUndefined(a) {
+  return typeof a === 'undefined';
+};
+var _isObject = function _isObject(a) {
+  return a !== null && typeof a === 'object';
+};
+
+// from https://github.com/npm-dom/is-dom/blob/master/index.js
+function isNode(val) {
+  if (!_isObject(val)) return false;
+  if (_isDefined(window) && _isObject(window.Node)) return val instanceof window.Node;
+  return 'number' == typeof val.nodeType && 'string' == typeof val.nodeName;
+}
+
+var useComputedStyles = _isDefined(window) && _isDefined(window.getComputedStyle);
+
+// Gets computed styles for an element
+// from https://github.com/jquery/jquery/blob/master/src/css/var/getStyles.js
+function getComputedStyles(node) {
+  if (useComputedStyles) {
+    var view = node.ownerDocument.defaultView;
+    if (!view.opener) view = window;
+    return view.getComputedStyle(node, null);
+  } else {
+    return node.currentStyle || node.style;
+  }
+}
+
+/**
+* Returns a collection of CSS property-value pairs
+* @param  {Element} node A DOM element to copy styles from
+* @param  {Object} [target] An optional object to copy styles to
+* @param {(Object|Boolean)} [default=true] A collection of CSS property-value pairs, false: copy none, true: copy all
+* @return {object} collection of CSS property-value pairs
+* @api public
+*/
+function computedStyles(node) {
+  var target = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+  var styleList = arguments.length <= 2 || arguments[2] === undefined ? true : arguments[2];
+
+  if (!isNode(node)) {
+    throw new Error('parameter 1 is not of type \'Element\'');
+  }
+
+  if (styleList === false) return target;
+
+  var computed = getComputedStyles(node);
+
+  if (styleList === true) {
+    var keysArray = useComputedStyles ? computed : Object.keys(computed);
+  } else {
+    var keysArray = Object.keys(styleList);
+  }
+
+  for (var i = 0, l = keysArray.length; i < l; i++) {
+    var key = keysArray[i];
+
+    var def = styleList === true || styleList[key];
+    if (def === false || _isUndefined(def)) continue; // copy never
+
+    var value = /* computed.getPropertyValue(key) || */computed[key]; // using getPropertyValue causes error in IE11
+    if (typeof value !== 'string' || value === '') continue; // invalid value
+
+    if (def === true || value !== def) {
+      // styleList === true || styleList[key] === true || styleList[key] !== value
+      target[key] = value;
+    }
+  }
+
+  return target;
+}
+
+// Removes attributes that are not valid for SVGs
+function cleanAttrs(el, attrs, styles) {
+  // attrs === false - remove all, attrs === true - allow all
+  if (attrs === true) {
+    return;
+  }
+
+  Array.prototype.slice.call(el.attributes).forEach(function (attr) {
+    // remove if it is not style nor on attrs  whitelist
+    // keeping attributes that are also styles because attributes override
+    if (attr.specified) {
+      if (attrs === '' || attrs === false || isUndefined(styles[attr.name]) && attrs.indexOf(attr.name) < 0) {
+        el.removeAttribute(attr.name);
+      }
+    }
+  });
+}
+
+function cleanStyle(tgt, parentStyles) {
+  if (tgt.style) {
+    inheritableAttrs.forEach(function (key) {
+      if (tgt.style[key] === parentStyles[key]) {
+        tgt.style.removeProperty(key);
+      }
+    });
+  }
+}
+
+function walker(attrs, defaultStyles) {
+  return function walk(src, tgt) {
+    if (!tgt.style) return;
+
+    computedStyles(src, tgt.style, defaultStyles);
+
+    var children = src.childNodes;
+    for (var i = 0; i < children.length; i++) {
+      walk(children[i], tgt.childNodes[i]);
+      cleanStyle(tgt.childNodes[i], tgt.style);
+    }
+
+    if (tgt.attributes) {
+      cleanAttrs(tgt, attrs, defaultStyles);
+    }
+  };
+}
+
+// Clones an SVGElement, copies approprate atttributes and styles.
+function cloneSvg(src, attrs, styles) {
+  var clonedSvg = src.cloneNode(true);
+  walker(attrs, styles)(src, clonedSvg);
+
+  return clonedSvg;
+}
+
+// inheritable styles may be overridden by parent, always copy for now
+inheritableAttrs.forEach(function (k) {
+  if (k in svgStyles) {
+    svgStyles[k] = true;
+  }
+});
+
+var SvgSaver = (function () {
+
+  /**
+  * SvgSaver constructor.
+  * @constructs SvgSaver
+  * @api public
+  *
+  * @example
+  * var svgsaver = new SvgSaver();                      // creates a new instance
+  * var svg = document.querySelector('#mysvg');         // find the SVG element
+  * svgsaver.asSvg(svg);                                // save as SVG
+  */
+
+  function SvgSaver() {
+    var _ref = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+
+    var attrs = _ref.attrs;
+    var styles = _ref.styles;
+
+    _classCallCheck(this, SvgSaver);
+
+    this.attrs = attrs === undefined ? svgAttrs : attrs;
+    this.styles = styles === undefined ? svgStyles : styles;
+  }
+
+  /**
+  * Return the SVG HTML text after cleaning
+  *
+  * @param {SVGElement} el The element to copy.
+  * @returns {String} SVG text after cleaning
+  * @api public
+  */
+
+  _createClass(SvgSaver, [{
+    key: 'getHTML',
+    value: function getHTML(el) {
+      var svg = cloneSvg(el, this.attrs, this.styles);
+
+      svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+      svg.setAttribute('version', 1.1);
+
+      // height and width needed to download in FireFox
+      svg.setAttribute('width', svg.getAttribute('width') || '500');
+      svg.setAttribute('height', svg.getAttribute('height') || '900');
+
+      return svg.outerHTML || new window.XMLSerializer().serializeToString(svg);
+    }
+
+    /**
+    * Return the SVG, after cleaning, as a text/xml Blob
+    *
+    * @param {SVGElement} el The element to copy.
+    * @returns {Blog} SVG as a text/xml Blob
+    * @api public
+    */
+  }, {
+    key: 'getBlob',
+    value: function getBlob(el) {
+      var html = this.getHTML(el);
+      return new Blob([html], { type: 'text/xml' });
+    }
+
+    /**
+    * Return the SVG, after cleaning, as a image/svg+xml;base64 URI encoded string
+    *
+    * @param {SVGElement} el The element to copy.
+    * @returns {String} SVG as image/svg+xml;base64 URI encoded string
+    * @api public
+    */
+  }, {
+    key: 'getUri',
+    value: function getUri(el) {
+      var html = this.getHTML(el);
+      if (isDefined(window.btoa)) {
+        return 'data:image/svg+xml;base64,' + window.btoa(html);
+      }
+      return 'data:image/svg+xml,' + encodeURIComponent(html);
+    }
+
+    /**
+    * Saves the SVG as a SVG file using method compatible with the browser
+    *
+    * @param {SVGElement} el The element to copy.
+    * @param {string} [filename] The filename to save, defaults to the SVG title or 'untitled.svg'
+    * @returns {SvgSaver} The SvgSaver instance
+    * @api public
+    */
+  }, {
+    key: 'asSvg',
+    value: function asSvg(el, filename) {
+      if (!filename || filename === '') {
+        filename = el.getAttribute('title');
+        filename = (filename || 'untitled') + '.svg';
+      }
+      if (isDefined(window.saveAs) && isFunction(Blob)) {
+        return saveAs(this.getBlob(el), filename);
+      } else {
+        return saveUri(this.getUri(el), filename);
+      }
+    }
+
+    /**
+    * Saves the SVG as a PNG file using method compatible with the browser
+    *
+    * @param {SVGElement} el The element to copy.
+    * @param {string} [filename] The filename to save, defaults to the SVG title or 'untitled.png'
+    * @returns {SvgSaver} The SvgSaver instance
+    * @api public
+    */
+  }, {
+    key: 'asPng',
+    value: function asPng(el, filename) {
+      if (!filename || filename === '') {
+        filename = el.getAttribute('title');
+        filename = (filename || 'untitled') + '.png';
+      }
+      return savePng(this.getUri(el), filename);
+    }
+  }]);
+
+  return SvgSaver;
+})();
+
+exports['default'] = SvgSaver;
+module.exports = exports['default'];
+
+},{}],2:[function(require,module,exports){
+var SvgSaver = require('svgsaver');
+
+var body = document.body;
+var svgsaver = new SvgSaver();
+
+initialize();
+
+function createPopover (source) {
+  var rect = source.getBoundingClientRect();
+
+  var buttonsContainer = document.createElement('div');
+  body.appendChild(buttonsContainer);
+
+  buttonsContainer.style.position = 'absolute';
+  buttonsContainer.style.top = (rect.top + body.scrollTop) + 'px';
+  buttonsContainer.style.left = (body.scrollLeft + rect.left) + 'px';
+
+  var button = document.createElement('button');
+  buttonsContainer.appendChild(button);
+  button.textContent = 'SVG';
+  button.addEventListener('click', function () {
+    svgsaver.asSvg(source);
+  });
+
+  button = document.createElement('button');
+  buttonsContainer.appendChild(button);
+  button.textContent = 'PNG';
+  button.addEventListener('click', function () {
+    svgsaver.asPng(source);
+  });
+}
+
+// from https://github.com/NYTimes/svg-crowbar/blob/gh-pages/svg-crowbar.js
+function initialize () {
+  var documents = [window.document];
+  var SVGSources = [];
+  var iframes = document.querySelectorAll('iframe');
+  var objects = document.querySelectorAll('object');
+
+  [].forEach.call(iframes, function (el) {
+    try {
+      if (el.contentDocument) {
+        documents.push(el.contentDocument);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  [].forEach.call(objects, function (el) {
+    try {
+      if (el.contentDocument) {
+        documents.push(el.contentDocument);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  documents.forEach(function (doc) {
+    var newSources = doc.querySelectorAll('svg');
+    for (var i = 0; i < newSources.length; i++) {
+      SVGSources.push(newSources[i]);
+    }
+  });
+  if (SVGSources.length > 0) {
+    SVGSources.forEach(createPopover);
+  }
+}
+
+},{"svgsaver":1}]},{},[2]);
